bccb4f71f787f3e60d81976609d2cff4b7880721
Xihaha <794140708@qq.com>
2024-11-17 18:14:50 +0800

    fix: 混淆

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/login',
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscatorPlugin({
+      options: {
+        debugProtection: true,
+        stringArray: true,
+        stringArrayEncoding: ['rc4', 'base64'],
+        stringArrayThreshold: 1,
+        rotateStringArray: true,
+        stringArrayWrappersCount: 2,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
